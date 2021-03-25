9553d49700863d141539e001b8c6b83026611e21
Hasan Mobarak <biplobjugp40@gmail.com>
2021-03-25 15:23:00 +0600

    google login popup error solved

--- src/app/components/my-account/my-account.component.ts
+++ src/app/components/my-account/my-account.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { AuthServiceService } from '../../_services/auth-service.service';
 import {Router} from '@angular/router';
 import {TokenStorageService } from '../../_services/token-storage.service';
+import { ViewChild,ElementRef } from '@angular/core'
 
 // https://technicaaadda.blogspot.com/2020/04/code-for-social-login.html
+// https://www.youtube.com/watch?v=S4rzmjuZWzI
+
+
 
 @Component({
   selector: 'app-my-account',
@@ -14,6 +18,9 @@ import {TokenStorageService } from '../../_services/token-storage.service';
 
 export class MyAccountComponent implements OnInit {
 
+  @ViewChild('loginRef', {static: true }) loginElement: ElementRef;
+  
+  auth2:any;
   form: any = {};
   isSuccessful = false;
   isLoggedIn = false;
@@ -23,13 +30,41 @@ export class MyAccountComponent implements OnInit {
 
   constructor( private router: Router) { }
 
-  ngOnInit() {
-    this.form = {
-      "firstName": "Mike",
-      "lastName": "Ross",
-      "email": "mike@test.com",
-      "password": "123"
-    };
+  ngOnInit(){
+    this.googleInitialize();
+  }
+
+
+  googleInitialize() {
+    window['googleSDKLoaded'] = () =>{
+      window['gapi'].load('auth2', () =>{
+        this.auth2 = window['gapi'].auth2.init({
+          client_id: '692072137787-7k2labjkfj2uvbt47k4ootvj3iil6n61.apps.googleusercontent.com',
+          cookie_policy: 'single_host_origin',
+          scope: 'profile email'
+        });
+        this.prepareLogin();
+      });
+    }
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'google-jssdk'));
+  }
+
+  prepareLogin() {
+    this.auth2.attachClickHandler(this.loginElement.nativeElement, {},
+      (googleUser) => {
+        let profile = googleUser.getBasicProfile();
+        console.log('Token || ' + googleUser.getAuthResponse().id_token);
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+      }, (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
   }
 /*
   signUp(){
